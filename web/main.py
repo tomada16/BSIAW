ec007046d4c76c0c41ff33777e0a2722888af2ab
tomada16 <tomadam1@poczta.onet.pl>
2025-10-11 12:11:08 +0200

    Added templates and styles
    Made a blank template for login, register and index html. Added css style for login page and changed login template.

--- web/main.py
+++ web/main.py
@@ -7,4 +7,16 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>witaj na mojej strone!!11!</p>"
+    return flask.render_template("index.html")
+
+@app.route("/login")
+def login():
+    return flask.render_template("login.html")
+
+@app.route("/register")
+def register():
+    return flask.render_template("register.html")
+
+# Need to run from IDE
+if __name__ == "__main__":
+    app.run(debug=True)
