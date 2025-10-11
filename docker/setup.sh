1bca0e1cf969246e30e8d66c71661f739a6656bd
tomada16 <tomadam1@poczta.onet.pl>
2025-10-11 13:22:13 +0200

    Added docker volume
    Added docker volume, excluded .env file and changed flask to run in debug mode

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,6 +2,6 @@
 # Run during container start.
 
 su postgres -c 'pg_ctl start -D /var/lib/postgresql/data'
-cd /srv/web && flask -A main run -h 0.0.0.0 -p 80 &
+cd /srv/web && flask -A main run -h 0.0.0.0 -p 80 --debug &
 
 tail -f /dev/null
