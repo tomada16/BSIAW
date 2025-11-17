ad6fcbe3bf0e61412e27c2d6f57b2de6453ea1ba
tomada16 <tomadam1@poczta.onet.pl>
2025-11-17 18:49:06 +0100

    added env variables

--- web/settings.py
+++ web/settings.py
@@ -1,10 +1,11 @@
 # Project settings
 # Copyright (c) 2025 Politechnika Wrocławska
+import os
 
 SESSION_TIMEOUT = 600
 MAX_MESSAGE_LEN = 2000
 
-DB_HOST = "db"
-DB_NAME = "bsiaw"
-DB_USER = "postgres"
-DB_PASS = ""
+DB_HOST = os.environ.get('DB_HOST', 'os')
+DB_NAME = os.environ.get('DB_NAME', 'bsiaw')
+DB_USER = os.environ.get('DB_USER', 'postgres')
+DB_PASS = os.environ.get('DB_PASS', '')
